6af7d87bf1b4577842cd37830b1c1e56c248d175
Paul Ulerick <p.ulerick17@gmail.com>
2022-12-30 17:52:55 -0500

    user feed posts are displaying properly

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { firestore } from '../firebase/FirebaseServer';
 import { collection, getDocs } from "firebase/firestore";
-
+import { nanoid } from 'nanoid';
 import Posts from './Posts';
 
 function Feed() {
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = async () => {
-    const query = await getDocs(collection(firestore, 'posts'));
-    
-    if (query) {
-      
-    } else {
-      console.log('no Posts');
-    }
+    // makes the reference for the firestore db
+    const col = collection(firestore, 'posts');
+    // grabs docs from posts and returns a promise
+    await getDocs(col)
+      .then((snapshot) => {
+        let post = [];
+        snapshot.docs.forEach((doc) => {
+          post.push({ ...doc.data(), id: doc.id })
+        })
+        setPosts(post)
+        console.log(Posts)
+      })
   };
 
   useEffect(() => {
     fetchPosts();
   }, []);
+
+  
+  
 
   return (
           posts.map(post => {
